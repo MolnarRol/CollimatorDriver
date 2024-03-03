86a7ed1d8df12250720b6e59e0da42a81d338782
Samuel Fertaľ <samofertal@gmail.com>
2024-03-03 14:52:53 +0100

    CRC core interface src - not functioning

---
+++ firmware/Modules/CRC/src/CRC.c
@@ -0,0 +1,68 @@
+/*
+ * CRC.c
+ *
+ *  Created on: 3. 3. 2024
+ *      Author: samof
+ */
+
+/*function for crc calculation*/
+
+#include <app_types.h>
+#include "CRC_core.h"
+
+
+U16 CRC_CrcCalculation_U16(const U16 *Data_U16, U16 DataLength_U16){    // length in 16bitovych chlievikov
+    U16 i=0;                                            /*index to for*/
+
+    U16 CRC_CrcResult_U16=0;                            /*initialization of return value variable*/
+
+    /*variables for splitting u16 16bits into 2 u16 8 bits number*/
+
+    U16 first_byte_U16=0;                               /*first byte variable*/
+    U16 second_byte_U16=0;                              /*second byte variable*/
+
+
+    while(DataLength_U16--){
+
+        /*U16 Data split*/
+
+        first_byte_U16=(*Data_U16) & 0xFF00;            /*first byte masking*/
+        second_byte_U16=(*Data_U16) & 0x00FF;           /*second byte masking*/
+        Data_U16++;
+
+        //CRC calculation for first byte
+
+        CRC_CrcResult_U16 ^= first_byte_U16;
+        for(i = (U16)0; i < (U16)8; i++){
+            if(CRC_CrcResult_U16 &= 0x80){              /*if CRC aligned perform XOR*/
+                CRC_CrcResult_U16 ^= (POLY<<1);         /*Align polynom then XOR*/
+            }
+            CRC_CrcResult_U16=CRC_CrcResult_U16 << (U16)1;   /*shift result*/
+        }
+
+        //CRC calculation for second byte
+
+        CRC_CrcResult_U16 ^= second_byte_U16;
+
+        for(i = (U16)0; i < (U16)8; i++){
+            if(CRC_CrcResult_U16 &= 0x80){              /*if CRC aligned perform XOR*/
+                CRC_CrcResult_U16 ^= (POLY<<1);         /*Align polynom then XOR*/
+            }
+            CRC_CrcResult_U16=CRC_CrcResult_U16 << (U16)1;   /*shift result*/
+        }
+    }
+
+    CRC_CrcResult_U16= CRC_CrcResult_U16>>(U16)2;            /*Neviem com ale mal to slapy*/
+
+    return CRC_CrcResult_U16;
+}
+
+U16 CRC_CrcVerificationReceive_U16(const U16 *Data_U16,  U16 DataLength_U16, U16 CRC_CrcCompare_U16){
+    U16 CRC_OwnResult_U16 = CRC_CrcCalculation_U16(Data_U16, DataLength_U16);
+
+    if(CRC_OwnResult_U16 == CRC_CrcCompare_U16){
+        return 0;
+    }
+    return 1;
+
+}
