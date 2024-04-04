80cad109f48b5ceca99bb0c713f4da5ceefcecea
Roland Molnar <roland.molnar.3@student.tuke.sk>
2024-04-04 10:53:26 +0200

    ATB without interrupt

--- firmware/InterruptServiceRoutines.c
+++ firmware/InterruptServiceRoutines.c
@@ -5,10 +5,12 @@
  *      Author: roland
  */
 #include <main.h>
+#include <ATB_interface.h>
 #include <TEST.h>
 
 inline void ISR_MotorControlHandler(void)
 {
+    ATB_IncrementTime();
     /* Motor control functionality. Called every PWM cycle. */
     TEST_ScalarMotorMovementHandler();
 }
