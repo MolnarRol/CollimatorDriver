39adecf6cc098a048248a2d2400e7d0b0f872d9b
Vadym Holysh <gusdima7@gmil.com>
2024-04-16 12:39:27 +0200

    labak

--- firmware/Modules/FieldOrientedControl/src/FOC.c
+++ firmware/Modules/FieldOrientedControl/src/FOC.c
@@ -64,13 +64,13 @@ void FOC_CalculateOutput(F32 ReferencePosition__rad__F32){
 //    }
 //
 //    /*compensation of nonlinearity in the id current component = Lq*p*iq*wr* */
-//    F32 CompensationCurrent_id = (F32) ( (F32)MOTOR_POLE_PAIRS_dU16 * ( MOTOR_INDUCTANCE__H__df32 / 2 ) * MDA_GetData_ps()->currents_s.iq__A__F32 * MDA_GetData_ps()->rotor_mech_speed__rad_s1__F32 );
+    F32 CompensationCurrent_id = (F32) ( (F32)MOTOR_POLE_PAIRS_dU16 * ( MOTOR_INDUCTANCE__H__df32 / 2 ) * MDA_GetData_ps()->currents_s.iq__A__F32 * MDA_GetData_ps()->rotor_mech_speed__rad_s1__F32 );
 //
 //    /*compensation of nonlinearity in the iq current component = Ld*p*id*wr */
-//    F32 CompensationCurrent_iq = (F32) ( (F32)MOTOR_POLE_PAIRS_dU16 * ( MOTOR_INDUCTANCE__H__df32 / 2 ) * MDA_GetData_ps()->currents_s.id__A__F32 * MDA_GetData_ps()->rotor_mech_speed__rad_s1__F32 );
+    F32 CompensationCurrent_iq = (F32) ( (F32)MOTOR_POLE_PAIRS_dU16 * ( MOTOR_INDUCTANCE__H__df32 / 2 ) * MDA_GetData_ps()->currents_s.id__A__F32 * MDA_GetData_ps()->rotor_mech_speed__rad_s1__F32 );
 //
 //    /*compensation of induced voltage = p*lambda*wr */
-//    F32 CompensationIndVoltage = (F32) ( (F32)MOTOR_POLE_PAIRS_dU16 * MOTOR_MAGNETIC_FLUX_OF_ROTOR_MAGNTES__Wb__df32 * MDA_GetData_ps()->rotor_mech_speed__rad_s1__F32);
+    F32 CompensationIndVoltage = (F32) ( (F32)MOTOR_POLE_PAIRS_dU16 * MOTOR_MAGNETIC_FLUX_OF_ROTOR_MAGNTES__Wb__df32 * MDA_GetData_ps()->rotor_mech_speed__rad_s1__F32);
 //
 //    /*Controllers*/
 //
@@ -81,18 +81,18 @@ void FOC_CalculateOutput(F32 ReferencePosition__rad__F32){
 //    PI_speed_controller.ref_value_f32 = Speed__rad_s__F32;
 //
 //    /* PI_iq_action = PI_speed_output */
-//    PI_iq_current_controller.action_value_f32 = PI_ctrl_CalculateOutput(&PI_speed_controller, MDA_GetData_ps()->rotor_mech_speed__rad_s1__F32);
+   PI_iq_current_controller.action_value_f32 = 0.5; //PI_ctrl_CalculateOutput(&PI_speed_controller, MDA_GetData_ps()->rotor_mech_speed__rad_s1__F32);
 //
 //    /* PI_iq_ref = acceleration * Jm *kt */
 //    PI_iq_current_controller.ref_value_f32 = Acceleration__rad_s_2__F32 * MOTOTR_MOMENT_OF_INERTIA__kg_m2__df32 * MOTOR_TORQUE_CONSTANT__Nm_A__df32;
 //
-    PI_id_current_controller.ref_value_f32 = 2;
+    PI_id_current_controller.ref_value_f32 = 0;
 
     /* ud = output from PI Controller id - compensation of nonlinearity in the current component id */
-    trans_s.dq_s.d_F32 = (F32)( ( PI_ctrl_CalculateOutput(&PI_id_current_controller, MDA_GetData_ps()->currents_s.id__A__F32) )); //- CompensationCurrent_id );
+    trans_s.dq_s.d_F32 = (F32)( ( PI_ctrl_CalculateOutput(&PI_id_current_controller, MDA_GetData_ps()->currents_s.id__A__F32) - CompensationCurrent_id ));
 
     /* uq = output from PI Controller iq - compensation of nonlinearity in the current component iq + compensation of induced voltage*/
-    trans_s.dq_s.q_F32 = 0.0;//(F32)( ( PI_ctrl_CalculateOutput(&PI_iq_current_controller, MDA_GetData_ps()->currents_s.iq__A__F32) ) + CompensationCurrent_iq + CompensationIndVoltage );
+    trans_s.dq_s.q_F32 = (F32)( ( PI_ctrl_CalculateOutput(&PI_iq_current_controller, MDA_GetData_ps()->currents_s.iq__A__F32) ) + CompensationCurrent_iq + CompensationIndVoltage );
 
     /* electric angle */
     trans_s.angle__rad__F32 = MDA_GetData_ps()->rotor_el_angle__rad__F32;
@@ -137,10 +137,12 @@ void CommutationAlignment(){
     EQep1Regs.QEPCTL.bit.SWI = 0;
     EDIS;
 
-    DELAY_US(100000);
+    DELAY_US(50000);
 
     PWM_SetCompareValues(0, 0, 0);
 
     PWM_SetOutputEnable(False_b);
+
+    DELAY_US(1000000);
 
 }
