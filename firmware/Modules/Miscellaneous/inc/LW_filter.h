a624631bb4adef903de85ec6848a827b2a834a44
Vadym Holysh <gusdima7@gmil.com>
2024-04-18 08:46:40 +0200

    current filters

--- firmware/Modules/Miscellaneous/inc/LW_filter.h
+++ firmware/Modules/Miscellaneous/inc/LW_filter.h
@@ -11,8 +11,8 @@
 #include "F2837xD_device.h"
 #include <main.h>
 
-#define SAMPLING_TIME__s__df32 ( (F32)1.0f )
-#define TIME_CONST_F__s__df32 ( (F32)1.0f )
+#define SAMPLING_TIME__s__df32 ( (F32)50.0e-6f )
+#define TIME_CONST_F__s__df32 ( (F32)250.0e-6f )
 #define A_CONST_df32 ( TIME_CONST_F__s__df32 / ( TIME_CONST_F__s__df32 + SAMPLING_TIME__s__df32 ) )
 
 F32 LW_Filter_id_CalculateOutput(F32 MeasuredData_F32);
--- firmware/Modules/Miscellaneous/src/LW_filter.c
+++ firmware/Modules/Miscellaneous/src/LW_filter.c
@@ -25,9 +25,9 @@ F32 LW_Filter_iq_CalculateOutput(F32 MeasuredData_F32){
 
 F32 LW_Filter_Speed_CalculateOutput(F32 MeasuredData_F32){
 
-    static F32 Output_Filter_F32 = 0;
-    Output_Filter_F32 = ( ( 1 - A_CONST_df32 ) * MeasuredData_F32 ) + (A_CONST_df32 * Output_Filter_F32);
+    static F32 Output_Filter_speed_F32 = 0;
+    Output_Filter_speed_F32 = ( ( 1 - A_CONST_df32 ) * MeasuredData_F32 ) + (A_CONST_df32 * Output_Filter_speed_F32);
 
-    return Output_Filter_F32;
+    return Output_Filter_speed_F32;
 }
 
